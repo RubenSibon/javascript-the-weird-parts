511ea5284101492c622ba5aa0ab1fd35a76b5089
Ruben Sibon <rubenjs@webricolage.nl>
2018-11-05 21:33:03 +0100

    Add src dir; minify html and css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,44 @@
-module.exports = function(grunt) {
+module.exports = grunt => {
+
     // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns.
     require('load-grunt-tasks')(grunt);
 
     // Project configuration.
-    grunt.initConfig({});
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css'
+                }]
+            }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    // includes files within path and its sub-directories
+                    {expand: true, cwd: 'src/', src: ['js/**'], dest: '.'}
+                ],
+            },
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'index.html': 'src/index.html'
+                }
+            }
+        }
+    });
 
     // Default task(s).
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['cssmin', 'copy', 'htmlmin']);
 
 };
